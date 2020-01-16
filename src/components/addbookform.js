9aa59d911e08c6a7744479ef82f983b0e76826be
OCTAVIAN <45077244+octaviandd@users.noreply.github.com>
2020-01-16 23:43:01 +0000

    Update addbookform.js

--- src/components/addbookform.js
+++ src/components/addbookform.js
@@ -1,17 +1,84 @@
 /** @format */
+/** @format */
 
 import React from "react";
 import Book from "./book";
 
 export default class Bookform extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "",
+      author: "",
+      isbn: "",
+      books: []
+    };
+    console.log(this.state);
+  }
+
+  updateTitle(e) {
+    const value = e.target.value;
+    this.setState(currentState => ({
+      title: value
+    }));
+  }
+
+  updateAuthor(e) {
+    const value = e.target.value;
+    this.setState(currentState => ({
+      author: value
+    }));
+  }
+
+  updateIsbn(e) {
+    const value = e.target.value;
+    this.setState(currentState => ({
+      isbn: value
+    }));
+    console.log(this.state);
+  }
+
+  Bookfactory(title, author, isbn) {
+    return {
+      title,
+      author,
+      isbn
+    };
+  }
+
+  addNewBook(e) {
+    e.preventDefault();
+    let title = this.state.title;
+    let author = this.state.author;
+    let isbn = this.state.isbn;
+    let book = this.Bookfactory(title, author, isbn);
+    this.state.books.push(book);
+  }
+
   render() {
     return (
       <div className="form-section">
-        <form>
-          <input type="text" placeholder="Title"></input>
-          <input type="text" placeholder="Author"></input>
-          <input type="text" placeholder="ISBN"></input>
-          <button>Add boook</button>
+        <form onSubmit={this.addNewBook.bind(this)}>
+          <input
+            type="text"
+            placeholder="Title"
+            value={this.state.title}
+            onChange={this.updateTitle.bind(this)}
+          ></input>
+          <input
+            type="text"
+            placeholder="Author"
+            value={this.state.author}
+            onChange={this.updateAuthor.bind(this)}
+          ></input>
+          <input
+            type="text"
+            value={this.state.isbn}
+            placeholder="ISBN"
+            onChange={this.updateIsbn.bind(this)}
+          ></input>
+          <input type="submit" value="Add Book"></input>
         </form>
       </div>
     );

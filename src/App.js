b15739cf4bd5cd6bef91c47178b11e4c46e056c9
OCTAVIAN <45077244+octaviandd@users.noreply.github.com>
2020-01-18 16:21:10 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Header from "./components/header";
 import Bookform from "./components/addbookform";
 import Booklist from "./components/booklist";
+import Showform from "./components/showform-button";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -13,28 +14,51 @@ export default class App extends React.Component {
     this.state = {
       books: [
         {
-          title: "How to code",
-          author: "Mark Marky",
-          isbn: "22323222"
+          title: "The Undiscovered Self",
+          author: "Carl Jung",
+          isbn: "8334012"
         },
         {
-          title: "how to make a booklist app",
-          author: "octavian david",
-          isbn: "23223223"
+          title: "The Republic",
+          author: "Plato",
+          isbn: "9382882"
         }
-      ]
+      ],
+      show: false
     };
   }
 
-  
+  showModal = e => {
+    this.setState({
+      show: true
+    });
+  };
 
+  callBackFunction = data => {
+    this.setState(currentState => {
+      currentState.books.push(data);
+      return {
+        books: currentState.books
+      };
+    });
+  };
+
+  removeBook = book => {
+    this.setState(currentState => {
+      currentState.books.splice(book, 1);
+      return {
+        book: currentState.books
+      };
+    });
+  };
 
   render() {
     return (
       <div>
         <Header />
-        <Booklist list={this.state.books} />
-        <Bookform />
+        <Booklist list={this.state.books} removeBook={this.removeBook} />
+        <Bookform callBack={this.callBackFunction} show={this.showModal} />
+        <Showform show={this.showModal} />
       </div>
     );
   }

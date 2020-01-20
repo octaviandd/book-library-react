25cb4dbbd0c1be06f1d37a1db668584a3749de8c
OCTAVIAN <45077244+octaviandd@users.noreply.github.com>
2020-01-20 12:25:48 +0000

    Update showform-button.js

--- src/components/showform-button.js
+++ src/components/showform-button.js
@@ -1,27 +1,82 @@
 /** @format */
-import React from "react";
 
-export default class Showform extends React.Component {
+import React from "react";
+import "./App.css";
+import Header from "./components/header";
+import Bookform from "./components/addbookform";
+import Booklist from "./components/booklist";
+import Showform from "./components/showform-button";
+
+export default class App extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+
+    this.state = {
+      books: [
+        {
+          title: "The Undiscovered Self",
+          author: "Carl Jung",
+          isbn: "8334012"
+        },
+        {
+          title: "The Republic",
+          author: "Plato",
+          isbn: "9382882"
+        }
+      ],
+      show: false,
+      read: false
+    };
   }
 
-  sendData = modal => {
-    this.props.callBack(modal);
+  callBackFunction = (data, modal) => {
+    this.setState(currentState => {
+      currentState.books.push(data);
+      return {
+        books: currentState.books,
+        show: modal
+      };
+    });
   };
 
-  callBack = () => {
-    this.sendData();
-    this.props.handleModal();
+  callBackFunction2 = modal => {
+    this.setState(currentState => {
+      return {
+        show: modal
+      };
+    });
+  };
+
+  handleModal = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+
+  removeBook = index => {
+    this.setState(currentState => {
+      currentState.books.splice(index, 1);
+      return {
+        books: currentState.books
+      };
+    });
   };
 
   render() {
     return (
-      <div className="show-button">
-        <button onClick={this.callBack}>
-          <i className="far fa-plus-square"></i>
-        </button>
+      <div>
+        <Header />
+        <Booklist list={this.state.books} removeBook={this.removeBook} />
+        <Bookform
+          callBack={this.callBackFunction}
+          modal={this.state.show}
+          closeModal={this.handleModal.bind(this)}
+        />
+        <Showform
+          show={this.state.show}
+          callBack={this.callBackFunction2}
+          handleModal={this.handleModal}
+        />
       </div>
     );
   }

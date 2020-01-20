44b60c87bda55c2c325b7860ed6f640a0fc3444e
OCTAVIAN <45077244+octaviandd@users.noreply.github.com>
2020-01-20 12:36:04 +0000

    Update booklist.js

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -8,21 +8,27 @@ function Booklist(props) {
         <table>
           <thead>
             <tr id="table-headers">
+              <th></th>
               <th>Title</th>
               <th>Author</th>
               <th>ISBN</th>
+              <th></th>
               <th></th>
             </tr>
           </thead>
           <tbody>
             {props.list.map((book, index) => (
               <tr key={index}>
+                <td>{index + 1}</td>
                 <td>{book.title}</td>
                 <td>{book.author}</td>
                 <td>{book.isbn}</td>
                 <td>
-                  <button onClick={() => props.removeBook(book)}>
-                    <i class="far fa-trash-alt"></i>
+                  <input type="checkbox"></input>
+                </td>
+                <td>
+                  <button onClick={() => props.removeBook(index)}>
+                    <i className="far fa-trash-alt"></i>
                   </button>
                 </td>
               </tr>
